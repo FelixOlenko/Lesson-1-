a35bba4aa0a2b4f973e257dedffc12b1f5380e18
Felix Olenko <0feliks0@gmail.com>
2022-06-04 14:23:55 +0300

    Hello_World

---
+++ EX1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EX_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" 2 Hello World! ");
+            Console.WriteLine(" 2 Hello World! ");
+            Console.WriteLine(" 2 Hello World! ");
+            Console.WriteLine(" 2 Hello World! ");
+            Console.WriteLine(" 2 Hello World! ");
+            Console.WriteLine(" 2 Hello World! ");
+        }
+    }
+}
