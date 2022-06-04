7f1fa1634bb0339be5ff6f589f330f078dd0674f
Felix Olenko <0feliks0@gmail.com>
2022-06-04 15:38:59 +0300

    Equal

---
+++ EX3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EX3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int NumberA = 52;
+            int NumberB = 512;
+            int result = NumberA + NumberB;
+            Console.WriteLine(result);
+        }
+    }
+}
