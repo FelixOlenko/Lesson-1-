05518a63b37b64294fd82ddfc811d77c10c08d59
Felix Olenko <0feliks0@gmail.com>
2022-06-04 15:53:23 +0300

    EX4

---
+++ EX4/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EX4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double NumberA = 12;
+            double NumberB = 5;
+            Console.WriteLine(NumberA / NumberB);
+        }
+    }
+}
