3025c22ecb9818c9d7a1bc9275d2a0e74e660bb3
Felix Olenko <0feliks0@gmail.com>
2022-06-05 22:44:06 +0300

    343

---
+++ EX6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace EX6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 5;
+            int c = 7;
+            int d = 3;
+            int e = 9;
+
+            int max = a;
+
+            if (a > max ) max = a;
+            if (b > max ) max = b;
+            if (c > max ) max = c;
+            if (d > max ) max = d;
+            if (e > max ) max = e;
+
+            Console.Write("max = ");
+            Console.WriteLine(max);
+        }
+    }
+}
