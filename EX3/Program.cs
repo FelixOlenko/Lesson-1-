94a6d73da1f9e85df5991f29aea2d622eb6e43fc
Felix Olenko <0feliks0@gmail.com>
2022-06-05 22:12:34 +0300

    12

--- EX3/Program.cs
+++ EX3/Program.cs
@@ -6,8 +6,10 @@ namespace EX3
     {
         static void Main(string[] args)
         {
-            int NumberA = 52;
-            int NumberB = 512;
+            int NumberA = new Random().Next(1, 10); // 1,2,3,4 .... 9
+            Console.WriteLine(NumberA);
+            int NumberB = new Random().Next(1, 10);
+            Console.WriteLine(NumberB);
             int result = NumberA + NumberB;
             Console.WriteLine(result);
         }
