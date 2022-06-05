941d1f6286bdec4be24bc599e92c8cffc7100164
Felix Olenko <0feliks0@gmail.com>
2022-06-05 22:30:08 +0300

    IMPEROR

---
+++ EX5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EX5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите имя пользователя: ");
+            string username = Console.ReadLine();
+
+            if (username.ToLower() == "феликс")
+            {
+                Console.WriteLine("Да здравствует его Императорское Величество Феликс Первый!");
+            }
+            else
+            {
+                Console.Write("Ну здарова...");
+                Console.WriteLine(username);
+            }
+
+
+
+        }
+    }
+}
